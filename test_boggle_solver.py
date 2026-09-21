286b6685dd956a2c87b0f0e25773729ab0b17d84
graceaflores-cs <flor.gracees@gmail.com>
2026-09-20 22:45:45 -0400

    Update test_boggle_solver.py

--- test_boggle_solver.py
+++ test_boggle_solver.py
@@ -1,28 +1,28 @@
 import unittest
 from boggle_solver import Boggle
 
+
+
 class TestBoggleSolver(unittest.TestCase):
 
-def test_empty_dictionary(self):
-grid = [["A"]]
-dictionary = []
+    def test_empty_dictionary(self):
+        grid = [["A"]]
+        dictionary = []
 
-game = Boggle(grid, dictionary)
+        game = Boggle(grid, dictionary)
 
-self.assertEqual([], game.getSolution())
+        self.assertEqual([], game.getSolution())
 
-def test_empty_grid(self):
-game = Boggle([], ["ABC"])
+    def test_empty_grid(self):
+        game = Boggle([], ["ABC"])
 
- 
-self.assertEqual([], game.getSolution())
+        self.assertEqual([], game.getSolution())
 
-def test_one_by_one_grid(self):
-game = Boggle([["A"]], ["ABC"])
- 
+    def test_one_by_one_grid(self):
+        game = Boggle([["A"]], ["ABC"])
 
-self.assertEqual([], game.getSolution())
+        self.assertEqual([], game.getSolution())
+
 
 if __name__ == "__main__":
-
-unittest.main()
+    unittest.main()
